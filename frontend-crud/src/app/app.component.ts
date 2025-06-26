3bd2b0a0647c4575fcaa546a758a4d5421210343
Agnes Gonçalves Barbosa <148410029+AgnesGB@users.noreply.github.com>
2025-06-26 14:40:57 +0000

    testando

--- frontend-crud/src/app/app.component.ts
+++ frontend-crud/src/app/app.component.ts
@@ -1,15 +1,13 @@
 // src/app/app.component.ts
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router'; // <-- Importe o RouterOutlet
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet], // <-- Adicione RouterOutlet aqui
-  template: `
-    <router-outlet></router-outlet>
-  `,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html', // APONTA PARA O ARQUIVO HTML
 })
 export class AppComponent {
   title = 'CRUD de Produtos com PrimeNG';
