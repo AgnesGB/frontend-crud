fd6f2c9dc8720a725bdd272214015ddf4cfc397b
Agnes Gonçalves Barbosa <148410029+AgnesGB@users.noreply.github.com>
2025-07-18 14:43:57 +0000

    Rotas

---
+++ frontend-crud/src/app/pages/index.ts
@@ -0,0 +1,4 @@
+// Pages
+export { ProductListPageComponent } from './product-list-page/product-list-page.component';
+export { ProductFormPageComponent } from './product-form-page/product-form-page.component';
+export { ProductDetailPageComponent } from './product-detail-page/product-detail-page.component';
